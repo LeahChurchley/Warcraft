ca04ced6da439b4e79c5943c4dcedc11aff41315
LeahChurchley <vagrant@lighthouse-labs>
2016-04-09 16:00:07 +0000

    Finished activity: all tests passed

--- lib/barracks.rb
+++ lib/barracks.rb
@@ -1,6 +1,46 @@
 class Barracks
+  attr_reader :gold, :food
+  
+  def initialize 
+    @gold = 1000
+    @food = 80
+  end
+
+  def train_footman
+    if can_train_footman?
+      @gold -= 135
+      @food -= 2
+      Footman.new
+    else
+      nil
+    end
+  end
+
+  def train_peasant
+    if can_train_peasant?
+      @gold -= 90
+      @food -= 5
+      Peasant.new
+    else
+      nil
+    end
+  end
+  
+
+  def can_train_peasant?
+    if gold >= 90 && food >= 5
+      true
+    else
+      false
+    end
+  end
 
   def can_train_footman?
-    true
+    if gold >= 135 && food >= 2
+      true
+    else
+      false
+    end
   end
+
 end
--- lib/footman.rb
+++ lib/footman.rb
@@ -1,13 +1,8 @@
 # http://classic.battle.net/war3/human/units/footman.shtml
 
-class Footman
-  DEFAULT_HEALTH = 60
-  DEFAULT_POWER = 10
-
-  attr_accessor :health_points, :attack_power
+class Footman < Unit
+  
   def initialize
-    @health_points = DEFAULT_HEALTH
-    @attack_power = DEFAULT_POWER
+    super(60, 10)
   end
-
 end
